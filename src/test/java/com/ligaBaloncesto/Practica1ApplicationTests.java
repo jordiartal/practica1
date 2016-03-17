27b89cad09bc038ad3cc0b20c89ed2eba3b31e0e
jordi.artal <joarol@gmail.com>
2016-03-17 08:02:00 +0100

    17/3/16

--- src/test/java/com/ligaBaloncesto/Practica1ApplicationTests.java
+++ src/test/java/com/ligaBaloncesto/Practica1ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Practica1Application.class)
+@SpringApplicationConfiguration(classes = LigaBaloncestoSpringApplication.class)
 @WebAppConfiguration
 public class Practica1ApplicationTests {
 
